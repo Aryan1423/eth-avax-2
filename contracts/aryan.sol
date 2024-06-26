e86a4404df5bd87b233e1790741b2696c0059bd2
Aryan Singh <113651343+Aryan1423@users.noreply.github.com>
2024-06-26 13:34:29 +0530

    Update aryan.sol

--- contracts/aryan.sol
+++ contracts/aryan.sol
@@ -3,27 +3,27 @@ pragma solidity ^0.8.18;
 contract TodoList {
     uint public taskCount;
     struct Task {
-        uint256 id;
+        uint id;
         string description;
         bool completed;
     }
 
-    mapping(uint256 => Task) public tasks;
+    mapping(uint => Task) public tasks;
     function addTask(string memory description) external {
         taskCount++;
         tasks[taskCount] = Task(taskCount, description, false);}
 
-    function getTask(uint256 id) external view returns (uint256, string memory, bool) {
+    function getTask(uint id) external view returns (uint, string memory, bool) {
         require(id > 0 && id <= taskCount, "Task ID is out of range.");
         Task memory task = tasks[id];
         return (task.id, task.description, task.completed);}
 
-    function completeTask(uint256 id) external {
+    function completeTask(uint id) external {
         require(id > 0 && id <= taskCount, "Task ID is out of range.");
         tasks[id].completed = true;
     }
 
-    function deleteTask(uint256 id) external {
+    function deleteTask(uint id) external {
         require(id > 0 && id <= taskCount, "Task ID is out of range.");
         delete tasks[id];
     }
